44275422643be41a2370861a904470d72966dd1c
Lior <levydlior@gmail.com>
2022-06-30 11:02:39 -0600

    commit tree 4496058985054172062312447d979c2029fd6fc0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,43 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
+  set :default_content_type, "application/json"
 
-  get '/games' do
+  get "/games" do
     games = Game.all.order(:title).limit(10)
     games.to_json
   end
 
-  get '/games/:id' do
+  get "/games/:id" do
     game = Game.find(params[:id])
 
     game.to_json(only: [:id, :title, :genre, :price], include: {
-      reviews: { only: [:comment, :score], include: {
-        user: { only: [:name] }
-      } }
-    })
+                   reviews: { only: [:comment, :score], include: {
+                     user: { only: [:name] },
+                   } },
+                 })
   end
 
+  delete "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  post "/reviews" do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id],
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
 end
